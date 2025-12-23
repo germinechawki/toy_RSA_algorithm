7380bbf23b5c677d1632b96b136f1f0809e32b7e
germinechawki <155088106+germinechawki@users.noreply.github.com>
2025-12-23 23:31:46 +0200

    Update rsa.h

--- src/core/rsa.h
+++ src/core/rsa.h
@@ -3,8 +3,9 @@
 
 #include <stdint.h>
 
-/* RSA operations */
-uint32_t rsa_encrypt(uint32_t plaintext, uint32_t e, uint32_t N);
-uint32_t rsa_decrypt(uint32_t ciphertext, uint32_t d, uint32_t N);
+uint32_t mont_pow(uint32_t base, uint32_t exp, uint32_t n);
 
-#endif /* RSA_H */
+uint32_t rsa_encrypt(uint32_t m, uint32_t e, uint32_t n);
+uint32_t rsa_decrypt(uint32_t c, uint32_t d, uint32_t n);
+
+#endif

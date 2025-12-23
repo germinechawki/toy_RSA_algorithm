0fa189866bae7b4df0c13e87f1e20fa15bc83559
germinechawki <155088106+germinechawki@users.noreply.github.com>
2025-12-23 23:30:49 +0200

    Update math.h

--- src/core/math.h
+++ src/core/math.h
@@ -3,13 +3,10 @@
 
 #include <stdint.h>
 
-/* Extended Euclidean Algorithm */
 int64_t egcd(int64_t a, int64_t b, int64_t *x, int64_t *y);
-
-/* Modular inverse */
 uint32_t modinv(uint32_t a, uint32_t m);
 
-/* Modular exponentiation */
-uint32_t modpow(uint32_t base, uint32_t exp, uint32_t mod);
+uint32_t mont_redc(uint64_t x, uint32_t n, uint32_t n_inv);
+uint32_t mont_mul(uint32_t a, uint32_t b, uint32_t n, uint32_t n_inv);
 
-#endif /* MATH_H */
+#endif
